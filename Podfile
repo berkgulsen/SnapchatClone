fd59c0db2592b3c4b914057456036eb537d0e806
berkgulsen <bipurpie.exe@gmail.com>
2023-10-14 00:03:33 +0300

    showing snaps with slider

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '14.0'
+# platform :ios, '14.0'
 
 target 'SnapchatClone' do
   # Comment the next line if you don't want to use dynamic frameworks
@@ -12,4 +12,13 @@ pod 'Firebase/Firestore'
 pod 'Firebase/Core'
 pod 'Firebase/Storage' 
 pod 'SDWebImage'
+pod 'ImageSlideshow'
+pod "ImageSlideshow/SDWebImage"
+end
+post_install do |pi|
+  pi.pods_project.targets.each do |t|
+    t.build_configurations.each do |config|
+      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '15.0'
+    end
+  end
 end
--- SnapchatClone/ViewControllers/SnapVC.swift
+++ SnapchatClone/ViewControllers/SnapVC.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import ImageSlideshow
 
 class SnapVC: UIViewController {
     
     var selectedSnap: Snap?
     var selectedTime: Int?
+    var inputArray = [SDWebImageSource]()
     
     @IBOutlet weak var timeLabel: UILabel!
     
@@ -22,6 +24,24 @@ class SnapVC: UIViewController {
         if let time = selectedTime {
             timeLabel.text = "Time Left: \(time)"
         }
+        
+        if let snap = selectedSnap {
+            for imageUrl in snap.imageUrlArray {
+                inputArray.append(SDWebImageSource(urlString: imageUrl)!)
+            }
+        }
+        
+        let imageSlideshow = ImageSlideshow(frame: CGRect(x: 10, y: 10, width: self.view.frame.width * 0.95, height: self.view.frame.height * 0.90))
+        imageSlideshow.backgroundColor = UIColor.white
+        
+        let pageIndicator = UIPageControl()
+        pageIndicator.currentPageIndicatorTintColor = UIColor.lightGray
+        pageIndicator.pageIndicatorTintColor = UIColor.black
+        imageSlideshow.pageIndicator = pageIndicator
+        
+        imageSlideshow.contentScaleMode = UIViewContentMode.scaleAspectFit
+        imageSlideshow.setImageInputs(inputArray)
+        self.view.addSubview(imageSlideshow)
     }
     
 
